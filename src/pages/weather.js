c7c970a39c367be622ab16bf227ad4d29a2b1247
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2023-01-24 20:49:33 -0500

    weather api call working

--- src/pages/weather.js
+++ src/pages/weather.js
@@ -1,8 +1,23 @@
 
 
 const Weather = () => {
+  const makeApiCall = async () =>{
+    try{
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.0636&lon=75.0786&appid=${process.env.openWeatherApiKey}`)
+      
+      const jsonResponse = await response.json()
+
+      console.log(jsonResponse)
+
+
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
-    <div>Weather</div>
+    <div>Weather
+      <button onClick={makeApiCall} >Make Api Call</button>
+    </div>
   )
 }
 
